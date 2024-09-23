2add4b8d69c92286d6cb5c3c1c9c142e03f1d21e
Atropo13 <119261084+Atropo13@users.noreply.github.com>
2024-09-23 09:06:56 +0000

    guardar

--- scr/piedra_papel_tijeras.py
+++ scr/piedra_papel_tijeras.py
@@ -1,1 +1,41 @@
-print("hola mundo")
+import random
+
+def ordenador_decide_jugada():
+    ''' 
+    Elige aleatoriamente entre piedra, papel o tijeras y devuelve la elección.     
+    '''
+    opciones = ["piedra", "papel", "tijeras"]
+    res = random.choice(opciones)
+    return res
+     
+def usuario_decide_jugada():
+    ''' 
+    Pide al usuario que elija entre piedra, papel o tijeras y devuelve la elección.     
+    '''
+    eleccion_usuario = input("Elige piedra, papel o tijeras: ")
+    while eleccion_usuario not in ["piedra", "papel", "tijeras"]:
+        eleccion_usuario = input("Opción no válida, por favor elige piedra, papel o tijeras: ")
+    return eleccion_usuario
+
+
+def determina_ganador(jugada_usuario, jugada_ordenador):
+    if jugada_usuario == jugada_ordenador:
+        return "Empate"
+    elif jugada_usuario == "piedra" and jugada_ordenador == "tijeras":
+        return "Ganaste"
+    elif jugada_usuario == "tijera" and jugada_ordenador == "papel":
+        return "Ganaste"
+    elif  jugada_usuario == "papel" and jugada_ordenador == "piedra":
+        return "Ganaste"
+    else:
+        return "Perdiste"
+    
+def jugar():
+    eleccion_ordenador = ordenador_decide_jugada()
+    print(eleccion_ordenador)
+    eleccion_jugador = usuario_decide_jugada()
+    victoria = determina_ganador(eleccion_jugador, eleccion_ordenador)
+    
+    print(victoria)
+if __name__ == "__main__":
+    jugar()
